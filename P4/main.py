1576fc910f51294a77c4066e781e28a83753e8fe
fernando-polo <fernandogmzmdo@gmail.com>
2025-02-16 15:06:35 -0600

    Endpoint - Crear nueva tarea
    Se creó el Endpoint de crear una nueva tarea

--- P4/main.py
+++ P4/main.py
@@ -1,51 +1,62 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from typing import Optional
+
 
 app = FastAPI(
-    title='To-do-List',
-    description='API - Lista de Tareas',
-    version='1.0'
+    title="To-do-List",
+    description="API - Lista de Tareas",
+    version="1.0"
 )
 
 tareas = [{
-            'id': 1,
-            'titulo': 'Estudiar para el examen',
-            'descripcion': 'Repasar los apuntes de TAI ',
-            'vencimiento': '14-02-24',
-            'Estado': 'completada'
+            "id": 1,
+            "titulo": "Estudiar para el examen",
+            "descripcion": "Repasar los apuntes de TAI ",
+            "vencimiento": "14-02-24",
+            "Estado": "completada"
           },
           {
-            'id': 2,
-            'titulo': 'Hacer chatbot de PONS',
-            'descripcion': 'Creación de chatbot',
-            'vencimiento': '14-02-24',
-            'Estado': 'completada'
+            "id": 2,
+            "titulo": "Hacer chatbot de PONS",
+            "descripcion": "Creación de chatbot",
+            "vencimiento": "14-02-24",
+            "Estado": "completada"
           },
           {
-            'id': 3,
-            'titulo': 'Repartir vistas de TAI',
-            'descripcion': 'Repartir entre todos los integrantes del equipo las vistas',
-            'vencimiento': '14-02-24',
-            'Estado': 'completada'
+            "id": 3,
+            "titulo": "Repartir vistas de TAI",
+            "descripcion": "Repartir entre todos los integrantes del equipo las vistas",
+            "vencimiento": "14-02-24",
+            "Estado": "completada"
           },
           {
-            'id': 4,
-            'titulo': 'Repartir vistas de Diseño de software',
-            'descripcion': 'Repartir entre todos los integrantes del equipo las vistas ',
-            'vencimiento': '14-02-24',
-            'Estado': 'completada'
+            "id": 4,
+            "titulo": "Repartir vistas de Diseño de software",
+            "descripcion": "Repartir entre todos los integrantes del equipo las vistas ",
+            "vencimiento": "14-02-24",
+            "Estado": "completada"
           },
 
 ]
 
 # Endpoint - Inicio
-@app.get('/')
+@app.get("/")
 def home():
-    return {'message':'hola mundo'}
+    return {"message":"hola mundo"}
 
 # Endpoint - Obtener todas las tareas
-@app.get('/tarea', tags=["Endpoints Principales"])
+@app.get("/tarea", tags=["Endpoints Principales"])
 def todas_las_tareas():
     return tareas
 
+# Endpoint - Crear nueva tarea
+@app.post("/tareaNueva/", tags=["Endpoints Principales"])
+def crear_nueva_tarea(tarea:dict):
+    for tar in tareas:
+        # if tar["id"] == tarea.get("id"):
+        if tar["id"] == tarea["id"]:
+          raise HTTPException(status_code=400, detail="Id ya existente.")
+    tareas.append(tarea)
+    return tarea
 
 
