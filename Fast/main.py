0ec1ed3c0d067c1f1536eb2732534e52c085a1df
fernando-polo <fernandogmzmdo@gmail.com>
2025-03-27 10:58:31 -0600

    P9:SQLAlchemy-Update&Delete
    Se crearon los endpoints para Actualizar y Eliminar usuarios, además el de Post y el de Get total y por ID (se me olvidó hacer el commit anterior)

--- Fast/main.py
+++ Fast/main.py
@@ -3,9 +3,12 @@ from typing import Optional, List
 from modelsPydantic import modeloUsuario, modeloAuth
 from genToken import createToken
 from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 from middlewares import BearerJWT
 from DB.conexion import Session, engine, Base
 from models.modelsDB import User
+
+
 
 app = FastAPI(
     title="Mi primer API 192",
@@ -25,10 +28,14 @@ usuarios = [
         {"id":4, "nombre":"Mariana", "edad":25, "correo":"mariana@gmail.com"},
 ]
 
+
+
 #Endpoint home
 @app.get('/', tags=['Hola mundo'])
 def home():
     return {'hello':'world fastAPI'}
+
+
 
 # Endpoint Autenticación
 @app.post('/auth/', tags=['Autentificación'])
@@ -41,35 +48,110 @@ def login(autorización:modeloAuth):
         return {"Aviso":"El usuario no está autorizado."}
 
 
-#Endpoint CONSULTA TODOS
-@app.get('/todosUsuarios', dependencies=[Depends(BearerJWT())], response_model = List[modeloUsuario], tags=['Operaciones CRUD'])
+
+#Endpoint Buscar un usuario por su ID
+@app.get('/usuariosGetID/{id}',response_model=modeloUsuario,  tags=['Operaciones CRUD'])
+def buscarUno(id: int):
+    db=Session()
+    try:
+        consultauno = db.query(User).filter(User.id == id).first()
+
+        if not consultauno:
+            return JSONResponse(status_code=404, content={'message':'Usuario no encontrado.'})
+        return JSONResponse(content=jsonable_encoder(consultauno))
+    
+    except Exception as e:
+        return JSONResponse(status_code=500, 
+                            content={'message':'Error al consultar usuario.', 
+                                     'Excepción':str(e)})
+    finally:
+        db.close()
+
+
+
+#Endpoint Consultar todos los usuarios
+# @app.get('/todosUsuarios', dependencies=[Depends(BearerJWT())], response_model = List[modeloUsuario], tags=['Operaciones CRUD'])
+@app.get('/usuariosGet',  response_model=modeloUsuario, tags=['Operaciones CRUD'])
 def leerUsuarios():
-    return usuarios
+    db=Session()
+    try:
+        consulta = db.query(User).all()
+        return JSONResponse(content=jsonable_encoder(consulta))
+    except Exception as e:
+        return JSONResponse(status_code=500, 
+                            content={'message':'Error al consultar usuarios.', 
+                                     'Excepción':str(e)})
+    finally:
+        db.close()
+
+
 
 # Endpoint - Agregar nuevos usuarios
-@app.post('/usuarios/', response_model=modeloUsuario, tags = ['Operaciones CRUD'])
+@app.post('/usuariosPost/', response_model=modeloUsuario, tags = ['Operaciones CRUD'])
 def agregarUsuario(usuario:modeloUsuario):
-    for usr in usuarios:
-        if usr["id"] == usuario.id:
-            raise HTTPException(status_code=400, detail='Id ya existente.')
-    usuarios.append(usuario)    
-    return usuario
+    db = Session()
+    try:
+        db.add(User(**usuario.model_dump()))
+        db.commit()
+        return JSONResponse(status_code=201, 
+                            content={'message':'Usuario guardado.', 
+                            'usuario':usuario.model_dump()  })
+    except Exception as e:
+        db.rollback()
+        return JSONResponse(status_code=500, 
+                            content={'message':'Error al guardar usuario.', 
+                                     'Excepción':str(e)})
+    finally:
+        db.close()
 
-# Endpoint - Modificar usuario
-@app.put('/usuariosPut/{id}', response_model=modeloUsuario, tags=['Operaciones CRUD'])
-def usuariosPut(id:int, usuarioActualizado:modeloUsuario):
-    for index, usr in enumerate(usuarios):
-        if usr["id"] == id:
-            usuarios[index]=usuarioActualizado.model_dump()
-            return usuarios[index]
-    raise HTTPException(status_code=400, detail="El id no existe")
+
+
+# Endpoint - Actualizar usuario
+@app.put('/usuariosPut/{id}',response_model=modeloUsuario,  tags=['Operaciones CRUD'])
+def modificarUsuario(id: int, usuario:modeloUsuario):
+    db=Session()
+    try:
+        consultauno = db.query(User).filter(User.id == id).first()
+
+        if not consultauno:
+            return JSONResponse(status_code=404, content={'message':'Usuario no encontrado.'})  
+        
+        for key, value in usuario.model_dump(exclude_unset=True).items():
+            setattr(consultauno, key, value)    
+
+        db.commit()
+        db.refresh(consultauno)    
+        return JSONResponse(content=jsonable_encoder(consultauno))
+    
+    except Exception as e:
+        return JSONResponse(status_code=500, 
+                            content={'message':'Error al consultar usuario.', 
+                                     'Excepción':str(e)})
+    finally:
+        db.close()
+
+
 
 # Endpoint - Eliminar usuario
-@app.delete('/usuarioDelete/{id}', tags=['Operaciones CRUD'])
-def usuariosDelete(id: int):
-    for index, usr in enumerate(usuarios):
-        if usr["id"] == id:
-            usuarios.pop(index) 
-            return {"message": "Usuario eliminado", "usuarios": usuarios}
-    raise HTTPException(status_code=404, detail='Id no existente.')
+@app.delete('/usuariosDelete/{id}',  tags=['Operaciones CRUD'])
+def eliminarUsuario(id: int):
+    db=Session()
+    try:
+
+        consultauno = db.query(User).filter(User.id == id).first()
+        if not consultauno:
+            return JSONResponse(status_code=404, content={'message':'Usuario no existente.'})
+        
+        db.delete(consultauno)  
+        db.commit()
+
+        consulta = db.query(User).all()
+
+        return JSONResponse(content={'message':'Usuario eliminado correctamente', 'base de datos actual: ':jsonable_encoder(consulta) })
     
+    except Exception as e:
+        return JSONResponse(status_code=500, 
+                            content={'message':'Error al encontrar al usuario.', 
+                                     'Excepción':str(e)})
+    finally:
+        db.close()
--- Fast/modelsPydantic.py
+++ Fast/modelsPydantic.py
@@ -1,10 +1,9 @@
 from pydantic import BaseModel, Field, EmailStr
 
 class modeloUsuario(BaseModel):
-    id:int = Field(...,gt=0, description="Id único y sólo números positivos.")
-    nombre:str = Field(...,min_length=3, max_length=85, description="Nombre con mínimo de 3 letras y máximo a 85.")
-    edad:int = Field(..., ge=1, le=120)
-    correo:EmailStr = Field(..., examples=["Usuario23@gmail.com"])
+    name:str = Field(...,min_length=3, max_length=85, description="Nombre con mínimo de 3 letras y máximo a 85.")
+    age:int = Field(..., ge=1, le=120)
+    email:EmailStr = Field(..., examples=["Usuario23@gmail.com"])
 
 
 class modeloAuth(BaseModel):
